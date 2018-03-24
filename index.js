c69390b482f50d4979a20d95de365e51fb72b947
lkminh <luongkienminh@gmail.com>
2018-03-24 20:49:48 +0700

    Add new_chat_members event

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 
+let welcomeMessage = null;
+
 // Matches "/echo [text]"
 bot.onText(/\/echo (.+)/, (message, match) => {
-  console.log('onText: ' + message);
+  console.log('onText: echo ');
 
   const chatId = message.chat.id;
   const text = match[1];
@@ -16,6 +18,28 @@ bot.onText(/\/echo (.+)/, (message, match) => {
   bot.sendMessage(chatId, `You sent *${text}*`, {
     parse_mode: 'Markdown',
   } );
+});
+
+bot.onText(/\/welcome (.+)/, (message, match) => {
+  const chatId = message.chat.id;
+  const text = match[1];	
+  let responseMessage = '';
+  if (text && text.length > 0) {
+  	welcomeMessage = text;
+  	responseMessage = 'Welcome message set!';
+  } else {
+  	responseMessage = 'Please specify a mesage';
+  }
+  bot.sendMessage(chatId, responseMessage);
+});
+
+bot.on('new_chat_members', (message, match) => {
+  const chatId = message.chat.id;
+  if (welcomeMessage !== null) {
+  	bot.sendMessage(chatId, welcomeMessage, {
+      parse_mode: 'Markdown',
+  	});	
+  }
 });
 
 console.log('ICOVN bot is ready');
