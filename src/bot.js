ea4f6370c481a5ff03221f4a5118f4142f2eb822
lkminh <luongkienminh@gmail.com>
2018-04-15 14:07:45 +0700

    fix welcome issue

--- src/bot.js
+++ src/bot.js
@@ -7,19 +7,23 @@ class Bot {
     }
     this._bot = new TelegramBot(config.token, {polling: true});
     this._welcomeMessage = config.welcomeMessage || null;
+    this._ownerId = parseInt(config.ownerId, 10);
     this.setupEvents();
   }
   setupEvents() {
     this._bot.onText(/\/welcome (.+)/, this.handleWelcomeCommand.bind(this));
     this._bot.on('new_chat_members', this.handleNewChatMembers.bind(this));
   }
-  handleWelcomeCommand(message, match) {
+  handleWelcomeCommand(message) {
     const chatId = message.chat.id;
-    const text = match[1];	
+    const senderId = message.from.id;
+    if (this._ownerId !== senderId) {
+    	return this.sendMessage(chatId, '`Forbidden!` Only the owner can set welcome message!');
+    }
+	const text = message.text.split('/welcome')[1];	
     if (text && text.length > 0) {
       this._welcomeMessage = text;
-      storage.setItem('welcomeMessage', text).then(() => {
-        console.log('set welcome message');
+      storage.setItem('--welcome-message--', text).then(() => {
         this.sendMessage(chatId, 'Welcome message set!');
       }).catch(() => {
         this.sendMessage(chatId, 'Failed to set welcome message');
@@ -30,14 +34,19 @@ class Bot {
   }
   handleNewChatMembers(message) {
     const chatId = message.chat.id;
-    const user = message.from;
+    const newMember = message.new_chat_member;
 
     if (this._welcomeMessage !== null) {
-      const welcomeMessage = `Hello *${user.first_name} ${user.last_name}*! ${this._welcomeMessage}`;
+      const welcomeMessage = this.parseText(newMember, this._welcomeMessage);
       this.sendMessage(chatId, welcomeMessage , {
         parse_mode: 'Markdown',
       });	
     }
+  }
+  parseText(user, text) {
+  	let result = text.split('$first_name').join(user.first_name);
+  	result = result.split('$last_name').join(user.last_name);
+  	return result;
   }
   sendMessage(chatId, message, options) {
     this._bot.sendMessage(chatId, message, options);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const Bot = require('./bot');
 const storage = require('./storage');
 const token = process.env.BOT_TOKEN;
+const ownerId = process.env.OWNER_ID;
 
 storage.init().then(() => {
-  const welcomeMessage = storage.getItemSync('welcomeMessage');
+  const welcomeMessage = storage.getItemSync('--welcome-message--');
   const bot = new Bot({
     token: token,
     welcomeMessage : welcomeMessage,
+    ownerId: ownerId,
   });
   console.log('ICOVN bot is ready');
 }).catch((e) => {
