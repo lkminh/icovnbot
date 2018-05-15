7a231fa843f04cb77b2abcbd86790d0b67ce3354
minhkl <minh.luong@8bitrockr.com>
2018-05-16 00:40:10 +0700

    update staic news

--- src/staticNews.js
+++ src/staticNews.js
@@ -1,16 +1,16 @@
-const emoji = require('node-emoji');
-
 const news = `
 All in:
 <b>ICOVN Group</b>
 
 Sau thành công từ dự án Merculet, mở bán trong vòng chỉ 2p đã bán sạch token, tiếp theo đây chúng tôi mang đến cho các ndt dự án HOT nhất VBB Egretia build về blockchain HTML5, theo dõi review đánh giá dự án tại đây: 
+
 https://icovn.net/phan-tich-dau-tu/danh-gia-ico-egretia-nen-tang-phat-trien-blockchain-html5/
 
 ✴✴✴Link đăng ký tham gia dự án:
+
 https://tokeneed.com/?refer=7a6ac72a2bcd60db
 
-💣💣💣 Link các sàn giao dịch , dự án có thể sẽ list :
+💣💣💣 Link các sàn giao dịch, dự án có thể sẽ list:
 
 1.Binance:
 https://www.binance.com/?ref=29215072
@@ -23,5 +23,10 @@ https://www.bitmart.com/?r=0J6gIa
 
 4.Bibox: 
 https://www.bibox.com/signPage?id=11362764&lang=en
+
+5.Kucoin:
+https://www.kucoin.com/#/?r=Rt2auQ
+
+Thân 😍😍😍
 `;
 module.exports = news;
