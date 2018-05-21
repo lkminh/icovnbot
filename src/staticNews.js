56c8828925e889b0a3d10bd99a314da0267d3930
minhkl <minh.luong@8bitrockr.com>
2018-05-21 20:16:08 +0700

    change static news

--- src/staticNews.js
+++ src/staticNews.js
@@ -1,31 +1,25 @@
 const news = `
-All in:
-<b>ICOVN Group</b>
-
-Sau thành công từ dự án Merculet, mở bán trong vòng chỉ 2p đã bán sạch token, tiếp theo đây chúng tôi mang đến cho các ndt dự án HOT nhất VBB Egretia build về blockchain HTML5, theo dõi review đánh giá dự án tại đây: 
+Sau thành công từ dự án Merculet , mở bán trong vòng chỉ 2p đã bán sạch token , tiếp theo đây chúng tôi mang đến cho các ndt dự án HOT nhất VBB Egretia build về blockchain HTML5 , theo dõi review đánh giá dự án tại đây : 
 
 https://icovn.net/phan-tich-dau-tu/danh-gia-ico-egretia-nen-tang-phat-trien-blockchain-html5/
 
-✴✴✴Link đăng ký tham gia dự án:
+✴✴✴Link đăng ký tham gia dự án :
 
 https://tokeneed.com/?refer=7a6ac72a2bcd60db
 
-💣💣💣 Link các sàn giao dịch, dự án có thể sẽ list:
+💣💣💣 Link các sàn giao dịch TOP đầu thế giới cho anh em Trader , Holder và Newbie 😋😋😋
 
-1.Binance:
-https://www.binance.com/?ref=29215072
+1.Binance : https://www.binance.com/?ref=29215072
 
-2.Huobi:
-https://www.huobi.br.com/en-us/topic/invited/?invite_code=mqia3
+2.Huobi : https://www.huobi.br.com/en-us/topic/invited/?invite_code=mqia3
 
-3.Bitmart: 
+3.Bitmart : 
 https://www.bitmart.com/?r=0J6gIa
 
-4.Bibox: 
+4.Bibox : 
 https://www.bibox.com/signPage?id=11362764&lang=en
 
-5.Kucoin:
-https://www.kucoin.com/#/?r=Rt2auQ
+5.Kucoin : https://www.kucoin.com/#/?r=Rt2auQ
 
 Thân 😍😍😍
 `;
